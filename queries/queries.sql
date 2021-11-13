3ba4545243348a1fcd15a4b54d9cac37e192572d
Jamie Pordoy <jamiepordoy@hotmail.com>
2021-11-13 23:20:21 +0000

    Create queries.sql

---
+++ queries/queries.sql
@@ -0,0 +1,3 @@
+SELECT HOUR(TIMEDIFF(end, start)) as `difference` FROM tbl_active_shifts
+
+SELECT tblusers.ID, tblusers.firstname, tblBookedShifts.bID, tblBookedShifts.shiftID, tblBookedShifts.status, tbl_active_shifts.salary, tbl_active_shifts.start, tbl_active_shifts.end,WEEKDAY(start),WEEKDAY(end) FROM tblusers INNER JOIN (tbl_active_shifts INNER JOIN tblBookedShifts ON tbl_active_shifts.shiftID = tblBookedShifts.shiftID) ON tblusers.ID = tblBookedShifts.id
