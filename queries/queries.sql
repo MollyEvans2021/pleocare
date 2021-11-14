ddd0f4ff78a2fc5dedee728f711f722d562c0c89
Jamie Pordoy <jamiepordoy@hotmail.com>
2021-11-14 00:39:02 +0000

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -4,3 +4,6 @@ SELECT tblusers.ID, tblusers.firstname, tblBookedShifts.bID, tblBookedShifts.shi
 
 SELECT tblusers.ID, tblusers.firstname, tblBookedShifts.bID, tblBookedShifts.shiftID, tblBookedShifts.status, tbl_active_shifts.salary, tbl_active_shifts.start, tbl_active_shifts.end,WEEKDAY(start),WEEKDAY(end), HOUR(TIMEDIFF(end, start)) as difference
 FROM tblusers INNER JOIN (tbl_active_shifts INNER JOIN tblBookedShifts ON tbl_active_shifts.shiftID = tblBookedShifts.shiftID) ON tblusers.ID = tblBookedShifts.id
+
+#Earnings
+SELECT shiftID, salary*difference AS total_price from Eearnings
